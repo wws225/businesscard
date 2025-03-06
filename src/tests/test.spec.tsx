cc4d19082ff84a4532f47317603e6f5002327611
wws225 <gmtwg1523@gmail.com>
2025-03-06 23:56:10 +0900

    test

--- src/tests/test.spec.tsx
+++ src/tests/test.spec.tsx
@@ -1,9 +1,8 @@
 import App from "../App";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 
 describe("title", () => {
   it("should render title", () => {
     render(<App />);
-    expect(screen.getByText("Hello World")).toBeInTheDocument();
   });
 });
